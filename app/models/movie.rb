5f37e8a7d8a9197e028aa6b5f580ca39b8690908
Ryan Horn <rj.horn@gmail.com>
2023-01-16 17:00:12 -0700

    done for now

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,31 @@
 class Movie < ActiveRecord::Base
+    def self.find_all_movies_by_year(year)
+        Movie.where(year: year)
+    end
 
+    ## CREATE
+    def Movie.create_with_title(title)
+        Movie.create(title:title)
+    end
+
+    def Movie.first_movie
+        Movie.first
+    end
+
+    def Movie.last_movie
+        Movie.last
+    end
+
+    def Movie.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def Movie.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+
+    def Movie.find_movies_after_2002()
+        Movie.where("release_date > 2002")
+    end
+    
 end
